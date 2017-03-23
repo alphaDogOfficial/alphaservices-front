87199fc03ef9c948fba7230d7602b4b471cfcd0d
Manuel Puyol <manuelpuyol@gmail.com>
2017-03-23 19:40:12 -0300

    ??

---
+++ server.js
@@ -0,0 +1,25 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 4000
+
+// using webpack-dev-server and middleware in development environment
+if(process.env.NODE_ENV !== 'production') {
+  var webpack = require('webpack');
+  var config = require('./webpack.config');
+  var compiler = webpack(config);
+}
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/dist/index.html')
+});
+
+app.listen(PORT, function(error) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+  }
+});
