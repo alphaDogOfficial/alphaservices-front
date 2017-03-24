4de15311327cbe7c60190caa7489aaaaba2c311c
Guilherme Mamprin <guilherme.mamprin@gmail.com>
2017-03-24 09:39:03 -0300

    7

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -1,6 +1,11 @@
-export default function routing($urlRouterProvider, $locationProvider, $stateProvider) {
+export default function routing($urlRouterProvider, $locationProvider, $stateProvider, $opbeatProvider) {
   $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise('/payment');
+
+  $opbeatProvider.config({
+        orgId: 'dd20edcfbeeb4f32b725f21a7b6a28f6',
+        appId: '38bf13e883'
+    });
   //
   // $stateProvider
   //   .state('content', {
--- src/app/app.js
+++ src/app/app.js
@@ -31,7 +31,7 @@ function initAnonyCart($localStorage) {
 
 const MODULE_NAME = 'app';
 
-angular.module(MODULE_NAME, [uirouter, modal, ngStorage, home, cart, login, auth, promocao, prodConstants, checkout, myAcc, cadastro, payment])
+angular.module(MODULE_NAME, [uirouter, modal, ngStorage, home, cart, login, auth, promocao, prodConstants, checkout, myAcc, cadastro, payment, 'ngOpbeat'])
   .config(routing)
   .controller('initAnonyCart', initAnonyCart)
   .controller('navbarCtrl', navbarCtrl);
