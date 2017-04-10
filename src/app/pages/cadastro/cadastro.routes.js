922a6e8cc68b94e2b78dd70b11d407708f980564
Guilherme Mamprin <guilherme.mamprin@gmail.com>
2017-04-10 10:03:55 -0300

    1

--- src/app/pages/cadastro/cadastro.routes.js
+++ src/app/pages/cadastro/cadastro.routes.js
@@ -1,6 +1,8 @@
 import '!ng-cache!../../components/navbar/navbar.html';
 import '!ng-cache!../../components/footer/footer.html';
 import '!ng-cache!./cadastro.html';
+import '!ng-cache!./login_page.html';
+
 
 export default function routes($stateProvider) {
   $stateProvider
