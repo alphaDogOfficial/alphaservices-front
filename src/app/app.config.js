fe1cb403ffbb7c659160c6d3625f92ed2f068eb0
Guilherme Mamprin <guilherme.mamprin@gmail.com>
2017-04-11 22:19:34 -0300

    1

--- src/app/app.config.js
+++ src/app/app.config.js
@@ -1,6 +1,6 @@
 export default function routing($urlRouterProvider, $locationProvider, $stateProvider, $opbeatProvider) {
   $locationProvider.html5Mode(true);
-  $urlRouterProvider.otherwise('/payment');
+  $urlRouterProvider.otherwise('/home');
 
   $opbeatProvider.config({
         orgId: 'dd20edcfbeeb4f32b725f21a7b6a28f6',
